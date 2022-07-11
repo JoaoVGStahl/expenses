8a57ecd45b496eec3b858695dfac562dfd2fafb6
João Girardi <joao.girardi@pontosys.com>
2022-07-11 15:41:58 -0300

    Versao Inicial!

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Despesas Pessoais"),
+      ),
+      body: Center(child: Text("Versão Inicial!")),
+    );
+  }
+}
