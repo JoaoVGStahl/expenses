170d41380f25cce97a6c258b9b5ac2015bdda385
João Girardi <joao.girardi@pontosys.com>
2022-07-12 08:23:24 -0300

    formatação Datas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -78,7 +79,7 @@ class MyHomePage extends StatelessWidget {
                             ),
                           ),
                           Text(
-                            tr.date.toString(),
+                            DateFormat('d MMM y').format(tr.date),
                             style: TextStyle(
                               color: Colors.grey,
                             ),
